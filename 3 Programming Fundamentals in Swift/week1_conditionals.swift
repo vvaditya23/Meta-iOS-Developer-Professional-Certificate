b4c25d06f71634b6398ef43fc84cda99b2f42cc5
Aditya Vyavahare <vvaditya23@icloud.com>
2023-07-28 15:54:15 +0530

    Create week1_conditionals.swift

---
+++ 3 Programming Fundamentals in Swift/week1_conditionals.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+let freeApp = true
+let morningTemperature = 70
+let eveningTemperature = 80
+let temperatureDegree = "Farenheit"
+
+if freeApp == true {
+    print("You are using the free version of the app. Buy the full version of the app to get access to all the features.")
+}
+
+if morningTemperature < eveningTemperature {
+    print("true")
+}
