1a3c7ee145349a7c555e5bd438b4af5e9bf7b640
Aditya Vyavahare <vvaditya23@icloud.com>
2023-07-29 23:01:15 +0530

    Create week1_loops.swift

---
+++ 3 Programming Fundamentals in Swift/week1_loops.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let levels = 10
+let freeLevels = 4
+let bonusLevel = 3
+for level in 1...levels {
+  if level == bonusLevel {
+    print("Skip bonus level \(bonusLevel).")
+    continue
+  }
+  print("Play level \(level).")
+  if level == freeLevels {
+    print("You have played all \(freeLevels) free levels. Buy the game to play the remaining \(levels - freeLevels) levels.")
+    break
+  }
+}
